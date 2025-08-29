5a8bfed24574de27704d6990612c19193eae58c1
James Beeson <jamesbeeson01@gmail.com>
2025-08-29 13:51:31 -0600

    Added Files

---
+++ notes.md
@@ -0,0 +1,2 @@
+Options for removing the title bar:
+- https://chatgpt.com/c/68b20035-ea08-8324-acff-b76e60097a49
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
 const path = require('node:path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -7,10 +7,15 @@ if (require('electron-squirrel-startup')) {
 }
 
 const createWindow = () => {
+  // Choose where the window displays on the screen
+  // const { screenwidth, screenheight } = screen.getPrimaryDisplay().workAreaSize;
+
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    // x: screenwidth / 2,
+    // y: screenheight / 2,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
@@ -41,11 +46,11 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit();
+//   }
+// });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
