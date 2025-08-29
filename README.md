12a283c4e158a4d4ded229dcdddbf088e9932d0b
James Beeson <jamesbeeson01@gmail.com>
2025-08-29 13:12:03 -0600

    Init README.md preload.js

---
+++ README.md
@@ -0,0 +1,8 @@
+# Main Hub
+
+Hub for custom programs, chats, and shortcuts on my desktop computer. Uses ElementJS to run.
+
+## Features
+
+Home screen opens using `ctrl + alt + H`
+From home screen, `ctrl + K` pull up a shortcut reference sheet
--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,10 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge } = require("electron");
+const {} = require("os");
+
+API = {
+    
+}
+
+contextBridge.exposeInMainWorld('preload', API);
